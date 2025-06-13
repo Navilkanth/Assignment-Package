8e576b6acb7e6e076c48df01ab5ed76ff028b836
Navil <Navil@DESKTOP-6CJ4T69>
2025-06-13 06:14:54 +0100

    this is my second java project

---
+++ C2TCjavaprogramming/src/day2/Javademoclass.java
@@ -0,0 +1,24 @@
+package day2;
+
+public class Javademoclass {
+
+	public static void main(String[] args) {
+		
+		char ch ='a';
+		System.out.println(ch);
+
+		char ch1=65;
+		System.out.println(ch1);
+
+		char var1='\u20AC';
+		System.out.println(var1);
+
+		char var2='\u08AC';
+		System.out.println(var2);
+
+		int a='A';
+		System.out.println(a);
+		
+	}
+
+}
